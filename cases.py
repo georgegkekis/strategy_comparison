40145622daf2b492791e780a5b2b9865d4b6eb96
George Gkekis <georgegkekis@gmail.com>
2026-01-27 12:52:26 +0000

    Add a list of the cases that will be researched

---
+++ cases.py
@@ -0,0 +1,87 @@
+CASES = [
+{"instrument":"S&P 500","ticker":"^GSPC","years":10,"dip":None},
+{"instrument":"S&P 500","ticker":"^GSPC","years":10,"dip":0.05},
+{"instrument":"S&P 500","ticker":"^GSPC","years":10,"dip":0.10},
+{"instrument":"S&P 500","ticker":"^GSPC","years":10,"dip":0.20},
+
+{"instrument":"S&P 500","ticker":"^GSPC","years":30,"dip":None},
+{"instrument":"S&P 500","ticker":"^GSPC","years":30,"dip":0.05},
+{"instrument":"S&P 500","ticker":"^GSPC","years":30,"dip":0.10},
+{"instrument":"S&P 500","ticker":"^GSPC","years":30,"dip":0.20},
+
+{"instrument":"Nasdaq 100","ticker":"^NDX","years":10,"dip":None},
+{"instrument":"Nasdaq 100","ticker":"^NDX","years":10,"dip":0.05},
+{"instrument":"Nasdaq 100","ticker":"^NDX","years":10,"dip":0.10},
+{"instrument":"Nasdaq 100","ticker":"^NDX","years":10,"dip":0.20},
+
+{"instrument":"Nasdaq 100","ticker":"^NDX","years":30,"dip":None},
+{"instrument":"Nasdaq 100","ticker":"^NDX","years":30,"dip":0.05},
+{"instrument":"Nasdaq 100","ticker":"^NDX","years":30,"dip":0.10},
+{"instrument":"Nasdaq 100","ticker":"^NDX","years":30,"dip":0.20},
+
+
+{"instrument":"Gold","ticker":"GLD","years":10,"dip":None},
+{"instrument":"Gold","ticker":"GLD","years":10,"dip":0.05},
+{"instrument":"Gold","ticker":"GLD","years":10,"dip":0.10},
+{"instrument":"Gold","ticker":"GLD","years":10,"dip":0.20},
+
+{"instrument":"Gold","ticker":"GLD","years":30,"dip":None},
+{"instrument":"Gold","ticker":"GLD","years":30,"dip":0.05},
+{"instrument":"Gold","ticker":"GLD","years":30,"dip":0.10},
+{"instrument":"Gold","ticker":"GLD","years":30,"dip":0.20},
+
+
+{"instrument":"Silver","ticker":"SLV","years":10,"dip":None},
+{"instrument":"Silver","ticker":"SLV","years":10,"dip":0.05},
+{"instrument":"Silver","ticker":"SLV","years":10,"dip":0.10},
+{"instrument":"Silver","ticker":"SLV","years":10,"dip":0.20},
+
+{"instrument":"Silver","ticker":"SLV","years":30,"dip":None},
+{"instrument":"Silver","ticker":"SLV","years":30,"dip":0.05},
+{"instrument":"Silver","ticker":"SLV","years":30,"dip":0.10},
+{"instrument":"Silver","ticker":"SLV","years":30,"dip":0.20},
+
+
+{"instrument":"Microsoft","ticker":"MSFT","years":10,"dip":None},
+{"instrument":"Microsoft","ticker":"MSFT","years":10,"dip":0.05},
+{"instrument":"Microsoft","ticker":"MSFT","years":10,"dip":0.10},
+{"instrument":"Microsoft","ticker":"MSFT","years":10,"dip":0.20},
+
+{"instrument":"Microsoft","ticker":"MSFT","years":30,"dip":None},
+{"instrument":"Microsoft","ticker":"MSFT","years":30,"dip":0.05},
+{"instrument":"Microsoft","ticker":"MSFT","years":30,"dip":0.10},
+{"instrument":"Microsoft","ticker":"MSFT","years":30,"dip":0.20},
+
+
+{"instrument":"Intel","ticker":"INTC","years":10,"dip":None},
+{"instrument":"Intel","ticker":"INTC","years":10,"dip":0.05},
+{"instrument":"Intel","ticker":"INTC","years":10,"dip":0.10},
+{"instrument":"Intel","ticker":"INTC","years":10,"dip":0.20},
+
+{"instrument":"Intel","ticker":"INTC","years":30,"dip":None},
+{"instrument":"Intel","ticker":"INTC","years":30,"dip":0.05},
+{"instrument":"Intel","ticker":"INTC","years":30,"dip":0.10},
+{"instrument":"Intel","ticker":"INTC","years":30,"dip":0.20},
+
+
+{"instrument":"Tesla","ticker":"TSLA","years":10,"dip":None},
+{"instrument":"Tesla","ticker":"TSLA","years":10,"dip":0.05},
+{"instrument":"Tesla","ticker":"TSLA","years":10,"dip":0.10},
+{"instrument":"Tesla","ticker":"TSLA","years":10,"dip":0.20},
+
+{"instrument":"Tesla","ticker":"TSLA","years":30,"dip":None},
+{"instrument":"Tesla","ticker":"TSLA","years":30,"dip":0.05},
+{"instrument":"Tesla","ticker":"TSLA","years":30,"dip":0.10},
+{"instrument":"Tesla","ticker":"TSLA","years":30,"dip":0.20},
+
+
+{"instrument":"Coca-Cola","ticker":"KO","years":10,"dip":None},
+{"instrument":"Coca-Cola","ticker":"KO","years":10,"dip":0.05},
+{"instrument":"Coca-Cola","ticker":"KO","years":10,"dip":0.10},
+{"instrument":"Coca-Cola","ticker":"KO","years":10,"dip":0.20},
+
+{"instrument":"Coca-Cola","ticker":"KO","years":30,"dip":None},
+{"instrument":"Coca-Cola","ticker":"KO","years":30,"dip":0.05},
+{"instrument":"Coca-Cola","ticker":"KO","years":30,"dip":0.10},
+{"instrument":"Coca-Cola","ticker":"KO","years":30,"dip":0.20},
+]
